1db8070cc2b07fe3a5bc69807546989c017fe437
ryzech <61711965+ryzech@users.noreply.github.com>
2021-05-07 13:56:44 -0500

    Add files via upload

---
+++ bluebubbles.rb
@@ -0,0 +1,12 @@
+cask "BlueBubbles" do
+  version "0.1.19"
+  sha256 :no_check
+
+  url "https://ryzech.net/BlueBubbles-{version}.dmg"
+  name "BlueBubbles"
+  desc "Bringing iMessage to Windows, Linux, and Android."
+  homepage "https://bluebubbles.app"
+
+  app "BlueBubbles.app"
+
+end
